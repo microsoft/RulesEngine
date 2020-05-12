e920138eddc649b33eaa9141eecb4cbd872570c5
Todd Meinershagen <todd@meinershagen.net>
2020-05-12 05:22:50 -0500

    Simplified the IRulesEngine interface to have a params array for Obje… (#14)
    * Simplified the IRulesEngine interface to have a params array for Objects and Rule Parameters.  Also updated the code to use the ILogger from Microsoft.Extensions.Logging.
    
    * Removed custom NullLogger and replaced with Microsoft NullLogger from Microsoft.Extensions.Logging.Abstractions package.

---
+++ src/RulesEngine/RulesEngine/Interfaces/IRulesEngine.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using RulesEngine.Models;
+using System.Collections.Generic;
+
+namespace RulesEngine.Interfaces
+{
+    public interface IRulesEngine
+    { 
+        /// <summary>
+        /// This will execute all the rules of the specified workflow
+        /// </summary>
+        /// <param name="workflowName">The name of the workflow with rules to execute against the inputs</param>
+        /// <param name="inputs">A variable number of inputs</param>
+        /// <returns>List of rule results</returns>
+        List<RuleResultTree> ExecuteRule(string workflowName, params object[] inputs);
+
+        /// <summary>
+        /// This will execute all the rules of the specified workflow
+        /// </summary>
+        /// <param name="workflowName">The name of the workflow with rules to execute against the inputs</param>
+        /// <param name="ruleParams">A variable number of rule parameters</param>
+        /// <returns>List of rule results</returns>
+        List<RuleResultTree> ExecuteRule(string workflowName, params RuleParameter[] ruleParams);
+    }
+}
