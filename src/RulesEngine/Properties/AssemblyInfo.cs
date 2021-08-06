519f07b92bfc7f317f74a0aab6b4b25c4dd05954
Abbas Cyclewala <abcy@microsoft.com>
2021-08-06 12:41:31 +0530

    Added strongname signing

--- src/RulesEngine/Properties/AssemblyInfo.cs
+++ src/RulesEngine/Properties/AssemblyInfo.cs
@@ -8,4 +8,4 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("RulesEngine.UnitTest")]
+[assembly: InternalsVisibleTo("RulesEngine.UnitTest, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c15956b2ac0945c55b69a185f5c3e02276693b0a5e42c8a1f08cb24e03dd87d91f9fa09f79b6b7b3aac4df46f2ea4ce4bfa31920bb0aad9f02793ab29de9fbf40f5ba9e347aa8569128459f31da1f6357eabe6e1308ac7c16b87a4d61e8d1785746a57ec67956d2e2454b3c98502a5d5c4a4168133bfaa431207c108efae03aa")]
