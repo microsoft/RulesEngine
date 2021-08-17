d262f7dc528275a5e7579e2e23deb67122f17e3a
Alex Reich <github@alexreich.com>
2021-08-17 10:15:09 -0700

    Revert "conventional progress"
    This reverts commit 9b404416b9d6b37632c58965ed9a557417f21d6b.

--- src/RulesEngine/Models/Workflow.cs
+++ src/RulesEngine/Models/Workflow.cs
@@ -19,11 +19,6 @@ namespace RulesEngine.Models
     public class Workflow
     {
         /// <summary>
-        /// Gets the workflow Id.
-        /// </summary>
-        public int Id { get; set; }
-
-        /// <summary>
         /// Gets the workflow name.
         /// </summary>
         public string WorkflowName { get; set; }
