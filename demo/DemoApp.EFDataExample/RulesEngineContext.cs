088c169635c39557d067b3ff311d376a58cfbae9
Aaron Sulwer <asulwer@gmail.com>
2022-01-22 15:01:51 -0700

    - updated nuget packages - altered code to support nuget packages that were updated

--- demo/DemoApp.EFDataExample/RulesEngineContext.cs
+++ demo/DemoApp.EFDataExample/RulesEngineContext.cs
@@ -29,16 +29,34 @@ namespace RulesEngine.Data
 
                 entity.Property(b => b.Properties)
                 .HasConversion(
-                    v => JsonSerializer.Serialize(v, null),
-                    v => JsonSerializer.Deserialize<Dictionary<string, object>>(v, null));
+                    v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+                    v => JsonSerializer.Deserialize<Dictionary<string, object>>(v, (JsonSerializerOptions)null));
 
                 entity.Property(p => p.Actions)
                 .HasConversion(
-                    v => JsonSerializer.Serialize(v, null),
-                   v => JsonSerializer.Deserialize<RuleActions>(v, null));
+                    v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+                   v => JsonSerializer.Deserialize<RuleActions>(v, (JsonSerializerOptions)null));
 
                 entity.Ignore(b => b.WorkflowsToInject);
             });
+
+            //modelBuilder.Entity<RulesEngine.Models.Rule>(entity => {
+            //    entity.HasKey(k => k.RuleName); //adds shadow property RuleName1
+            //    entity.Ignore(b => b.WorkflowsToInject);
+            //    entity.Ignore(b => b.WorkflowRulesToInject); //has a get which is why this line is necessary
+
+            //    entity.Property(b => b.Properties).HasConversion(
+            //        v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+            //        v => JsonSerializer.Deserialize<Dictionary<string, object>>(v, (JsonSerializerOptions)null),
+            //        new ValueComparer<Dictionary<string, object>>(
+            //            (c1, c2) => c1.SequenceEqual(c2),
+            //            c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+            //            c => c));
+
+            //    entity.Property(p => p.Actions).HasConversion(
+            //        v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+            //        v => JsonSerializer.Deserialize<RulesEngine.Models.RuleActions>(v, (JsonSerializerOptions)null));                
+            //});
         }
     }
 
