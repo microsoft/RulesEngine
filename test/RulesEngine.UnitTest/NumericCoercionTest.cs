c5eb66d6e768eddb08bcad951ee5732a8fd8e3d0
Steven Frew <steven.fullhouse@gmail.com>
2022-11-27 08:57:52 +0000

    cleaned up tests

--- test/RulesEngine.UnitTest/NumericCoercionTest.cs
+++ test/RulesEngine.UnitTest/NumericCoercionTest.cs
@@ -52,54 +52,47 @@ namespace RulesEngine.UnitTest {
 			var engine = new RulesEngine();
 			engine.AddOrUpdateWorkflow(workflow);
 
-			{
-				// This data should PASS. Rule requires at least one value < -5.6
-				dynamic passDataStaticArray = new ExpandoObject();
-				passDataStaticArray.values = new[] { -1, -2, -3, -4, -5, -5.499, -5.5, -5.61 };
-				// Passing 0 for the TypeDetectionSampleSize means "sample all data please".
-				var input_pass_static = new RuleParameter("test", passDataStaticArray, 0);
-
-				// This data should FAIL. Rule requires at least one value < -5.6
-				dynamic failDataStaticArray = new ExpandoObject();
-				failDataStaticArray.values = new[] { -1, -2, -3, -4, -5, -5.499, -5.5 };
-				// Passing 0 for the TypeDetectionSampleSize means "sample all data please".
-				var input_fail_static = new RuleParameter("test", failDataStaticArray, 0);
-
-				var pass_results_static = await engine.ExecuteAllRulesAsync(workflowName, input_pass_static);
-				var fail_results_static = await engine.ExecuteAllRulesAsync(workflowName, input_fail_static);
-				Assert.True(pass_results_static.First().IsSuccess);
-				Assert.False(fail_results_static.First().IsSuccess);
+			async Task RunTest(object inputValues, int sampleSize, bool expectedToPass) {
+				dynamic inputDataObject = new ExpandoObject();
+				inputDataObject.values = inputValues;
+				var inputDynamic = new RuleParameter("test", inputDataObject, sampleSize);
+				var result = await engine.ExecuteAllRulesAsync(workflowName, inputDynamic);
+				Assert.True(result.First().IsSuccess == expectedToPass);
 			}
 
-			async Task RunDynamicTest(List<dynamic> passData, List<dynamic> failData) {
-				// This data should PASS. Rule requires at least one value < -5.6
-				dynamic passDataDynamicArray = new ExpandoObject();
-				passDataDynamicArray.values = passData;
-				// Passing 0 for the TypeDetectionSampleSize means "sample all data please".
-				var input_pass_dynamic = new RuleParameter("test", passDataDynamicArray, 0);
+			// Using a non-dynamic array will work. The runtime will treat this as an array of
+			// doubles. Even though we are only using a sample size of 1, that is enough to
+			// convince the engine that the collection is a collection of doubles.
+			// So this will PASS since there ARE values < -5.6
+			await RunTest(new[] { -1, -2, -3.0, -4.0, -5.0, -5.499, -5.5, -5.60001 }, 1, true);
+			// And this will FAIL since there are NO values < -5.6
+			await RunTest(new[] { -1, -2, -3.0, -4.0, -5.0, -5.499, -5.5 }, 1, false);
 
-				// This data should FAIL. Rule requires at least one value < -5.6
-				// BUT ... without the coercion, it will PASS, as the array will become an int array,
-				// and -5.5 will, weirdly, become -6, even though "(int)-5.5" is "-5".
-				// This is because the Utils code uses Convert.ChangeType(), which will
-				// convert -5.5 to -6.
-				dynamic failDataDynamicArray = new ExpandoObject();
-				failDataDynamicArray.values = failData;
-				// Passing 0 for the TypeDetectionSampleSize means "sample all data please".
-				var input_fail_dynamic = new RuleParameter("test", failDataDynamicArray, 0);
+			// This test should PASS. We are passing a sampleSize of 1, which should mean that the
+			// type detection code will look at only the first list element (an int), and convert
+			// the entire list to ints. Subsequently, the -5.5 value (which should make the "< -5.5"
+			// condition FAIL) will be converted to -6, causing it to PASS.
+			// Note that -5.5 will, weirdly, become -6, even though a simple explicit cast of
+			// "(int)-5.5" is actually "-5". This is because the Utils code uses
+			// Convert.ChangeType(), which does a different style of conversion, and will convert
+			// -5.5 to -6 for some reason.
+			await RunTest(new List<dynamic> { -1, -2, -3.0, -4.0, -5.0, -5.499, -5.5 }, 1, true);
+			// Same test, but with a sample size of 0 (meaning "sample all data"). Since later
+			// values are doubles, this will treat all values as doubles, and so we will get the
+			// correct result of FAIL.
+			await RunTest(new List<dynamic> { -1, -2, -3.0, -4.0, -5.0, -5.499, -5.5, -5.5 }, 0, false);
 
-				var pass_results_dynamic = await engine.ExecuteAllRulesAsync(workflowName, input_pass_dynamic);
-				var fail_results_dynamic = await engine.ExecuteAllRulesAsync(workflowName, input_fail_dynamic);
-				Assert.True(pass_results_dynamic.First().IsSuccess);
-				Assert.False(fail_results_dynamic.First().IsSuccess);
-			}
-
-			// Simple numeric coercion test.
-			// Type detected should be "double".
-			await RunDynamicTest(new List<dynamic> { -1, -2, -3.0, -4.0, -5.0, -5.499, -5.5, -5.61 }, new List<dynamic> { -1, -2, -3.0, -4.0, -5.0, -5.499, -5.5 });
-			// Check that we can add a null into the mix okay.
-			// Type detected should be "System.Nullable<System.Double>"
-			await RunDynamicTest(new List<dynamic> { -1, -2, -3.0, null, -4.0, -5.0, -5.499, -5.5, -5.61 }, new List<dynamic> { -1, -2, -3.0, null, -4.0, -5.0, -5.499, -5.5 });
+			// Now we'll throw in a null value.
+			// Again, this test will sample only the first element (an int), which will then make
+			// the code attempt to convert the entire list to ints. This will throw an exception
+			// when it hits the null.
+			await Assert.ThrowsAsync<System.NotSupportedException>(async () => await RunTest(new List<dynamic> { -1, -2, -3.0, null, -4.0, -5.0, -5.499, -5.5, -5.5 }, 1, false));
+			// But by checking the entire set of data, the type detection will decide that the
+			// data should be treated as System.Nullable<System.Double>.
+			// This test should FAIL simply because it doesn't have a value < -5.6
+			await RunTest(new List<dynamic> { -1, -2, -3.0, null, -4.0, -5.0, -5.499, -5.5, -5.5 }, 0, false);
+			// This test should PASS because it DOES have a value < -5.6
+			await RunTest(new List<dynamic> { -1, -2, -3.0, null, -4.0, -5.0, -5.499, -5.5, -5.5, -5.60001 }, 0, true);
 		}
 	}
 }
