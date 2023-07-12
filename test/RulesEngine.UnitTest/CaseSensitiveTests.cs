a28ad437e8958578527b303d11119d51da81cd7f
Abbas Cyclewala <abcy@microsoft.com>
2023-07-12 16:22:12 +0530

    updated test cases

--- test/RulesEngine.UnitTest/CaseSensitiveTests.cs
+++ test/RulesEngine.UnitTest/CaseSensitiveTests.cs
@@ -4,6 +4,7 @@
 using RulesEngine.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using Xunit;
 
 namespace RulesEngine.UnitTest
 {
+    [ExcludeFromCodeCoverage]
     public class CaseSensitiveTests
     {
         [Theory]
