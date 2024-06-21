489c8251039b78e9d05cd9dc204e17bec1c5dd6d
Tim Wilke <dev@timwilke.de>
2024-06-21 15:20:56 +0200

    Update AssemblyInfo.cs

--- src/RulesEngine/Properties/AssemblyInfo.cs
+++ src/RulesEngine/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 [assembly:
     InternalsVisibleTo(
-        "RulesEngine.UnitTest, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c15956b2ac0945c55b69a185f5c3e02276693b0a5e42c8a1f08cb24e03dd87d91f9fa09f79b6b7b3aac4df46f2ea4ce4bfa31920bb0aad9f02793ab29de9fbf40f5ba9e347aa8569128459f31da1f6357eabe6e1308ac7c16b87a4d61e8d1785746a57ec67956d2e2454b3c98502a5d5c4a4168133bfaa431207c108efae03aa")]
+        "RulesEngine.UnitTest, PublicKey=00240000048000009400000006020000002400005253413100040000010001009fff9dd6911b48b7ea1488cdf394d4133c92d96f7240a4906dbd0edfb3f1a93d6fac448ffad397e959923bad15ba60ba911eaa3cb923efd043c278fdd690cd9198071e8580417525a8e4b2910f55926bfe27245a37313242110b1230a4b074047bba2a617a68b4c5f4e14d380d23a6698ca8596713473021c3eb419e4879cac2")]
