854736015a5d5449fef4fc9d8c59f1a97a9cf5cd
Aaron Sulwer <asulwer@outlook.com>
2024-06-20 17:25:37 -0700

    removed Init as it seemed redundant

--- demo/DemoApp/Program.cs
+++ demo/DemoApp/Program.cs
@@ -18,10 +18,10 @@ namespace DemoApp
                 //initiate cancel, set to something lower than time it takes for program to complete to initiate cancel
                 //cts.CancelAfter(TimeSpan.FromMilliseconds(94));
 
-                await new Demos.Basic().Run(cts.Token);
-                await new Demos.JSON().Run(cts.Token);
-                await new Demos.NestedInput().Run(cts.Token);
-                await new Demos.EF().Run(cts.Token);
+                //await new Demos.Basic().Run(cts.Token);
+                //await new Demos.JSON().Run(cts.Token);
+                //await new Demos.NestedInput().Run(cts.Token);
+                //await new Demos.EF().Run(cts.Token);
                 await new Demos.UseFastExpressionCompiler().Run(cts.Token);
             }
 
--- src/RulesEngine/Models/RuleParameter.cs
+++ src/RulesEngine/Models/RuleParameter.cs
@@ -5,22 +5,20 @@ using RulesEngine.HelperFunctions;
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace RulesEngine.Models
 {
     [ExcludeFromCodeCoverage]
     public class RuleParameter
     {
-        public RuleParameter(string name, object value)
+        public RuleParameter(string name, object value) : this(name, value?.GetType(), value) { }       
+        protected RuleParameter(string name, Type type, object value = null)
         {
+            Name = name;
             Value = Utils.GetTypedObject(value);
-            Init(name, Value?.GetType());
-        }
-       
-        internal RuleParameter(string name, Type type,object value = null)
-        {
-            Value = Utils.GetTypedObject(value);
-            Init(name, type);
+            Type = type ?? typeof(object);
+            ParameterExpression = Expression.Parameter(Type, Name);
         }
 
         public Type Type { get; private set; }
@@ -28,17 +26,11 @@ namespace RulesEngine.Models
         public object Value { get; private set; }
         public ParameterExpression ParameterExpression { get; private set; }
 
-        private void Init(string name, Type type)
-        {
-            Name = name;
-            Type = type ?? typeof(object);
-            ParameterExpression = Expression.Parameter(Type, Name);
-        }
-
         public static RuleParameter Create<T>(string name, T value)
         {
             var typedValue = Utils.GetTypedObject(value);
             var type = typedValue?.GetType() ?? typeof(T);
+
             return new RuleParameter(name,type,value);
         }
     }
