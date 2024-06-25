430faa61417ad474c95f464cdbba415249f9fb6a
Tim <tim.wilke91@gmail.com>
2024-06-25 21:18:17 +0200

    AddedTestForUpdate

--- test/RulesEngine.UnitTest/RuleExpressionBuilderFactoryTest.cs
+++ test/RulesEngine.UnitTest/RuleExpressionBuilderFactoryTest.cs
@@ -6,6 +6,7 @@ using RulesEngine.Models;
 using System;
 using System.Diagnostics.CodeAnalysis;
 using Xunit;
+using FluentValidation;
 
 namespace RulesEngine.UnitTest;
 
@@ -24,5 +25,33 @@ public class RuleExpressionBuilderFactoryTest
 
         var builderType = builder.GetType();
         Assert.Equal(expectedExpressionBuilderType.ToString(), builderType.ToString());
+    }
+
+    [Fact]
+    public void SystemLinqDynamicCore_WithLiterals_ParsingCorretly()
+    {
+        var board = new { NumberOfMembers = default(decimal?) };
+
+        var parameter = RuleParameter.Create("Board", board);
+
+        var parser = new RuleExpressionParser();
+
+        try
+        {
+            const string expression1 = "Board.NumberOfMembers = 0.2d";
+            var result1 = parser.Evaluate<bool>(expression1, [parameter]);
+            Assert.False(result1);
+        }
+        catch (Exception)
+        {
+            // passing it over.
+        }
+
+        // This will throw an exception even if the expression is valid,
+        // because the first one executed and cached in the Literals Dictionary.
+        // This should not throw as it's valid.
+        const string expression2 = "Board.NumberOfMembers = 0.2";
+        var result2 = parser.Evaluate<bool>(expression2, [parameter]);
+        Assert.False(result2);
     }
 }
