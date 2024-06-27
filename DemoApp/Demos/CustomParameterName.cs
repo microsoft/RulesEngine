87bed717dbbc7d7c8f46933b99e642b14acaf6a9
Aaron Sulwer <asulwer@gmail.com>
2024-06-27 15:33:07 -0700

    added demo code to support solution for https://github.com/microsoft/RulesEngine/issues/581

---
+++ DemoApp/Demos/CustomParameterName.cs
@@ -0,0 +1,55 @@
+﻿using RulesEngine.Models;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using static RulesEngine.Extensions.ListofRuleResultTreeExtension;
+
+namespace DemoApp.Demos
+{
+    public class CustomParameterName
+    {
+        public async Task Run(CancellationToken ct = default)
+        {
+            Console.WriteLine($"Running {nameof(Basic)}....");
+
+            var workflows = new Workflow[] {
+                new Workflow {
+                    WorkflowName = "my_workflow",
+                    Rules = [
+                        new Rule {
+                            RuleName = "MatchesFabrikam",
+                            SuccessMessage = "does match",
+                            ErrorMessage = "does not match",
+                            Expression = "myValue.Value1 == \"Fabrikam\""
+                        }
+                    ]
+                }
+            };
+
+            var bre = new RulesEngine.RulesEngine(workflows);
+
+            var rp = new RuleParameter[] {
+                new RuleParameter("myValue", new { Value1 = "Fabrikam" })
+            };
+
+            await foreach (var async_rrt in bre.ExecuteAllWorkflows(rp, ct))
+            {
+                var outcome = false;
+
+                //Different ways to show test results:
+                outcome = async_rrt.TrueForAll(r => r.IsSuccess);
+
+                async_rrt.OnSuccess((eventName) => {
+                    Console.WriteLine($"Result '{eventName}' is as expected.");
+                    outcome = true;
+                });
+
+                async_rrt.OnFail(() => {
+                    outcome = false;
+                });
+
+                Console.WriteLine($"Test outcome: {outcome}.");
+            }
+        }
+    }
+}
--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -24,6 +24,7 @@ namespace DemoApp
                 await new Demos.EF().Run(cts.Token);
                 await new Demos.UseFastExpressionCompiler().Run(cts.Token);
                 await new Demos.MultipleWorkflows().Run(cts.Token);
+                await new Demos.CustomParameterName().Run(cts.Token);
             }
 
             DateTime end = DateTime.Now;
