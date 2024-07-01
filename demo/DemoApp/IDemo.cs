0c3cb4d4c69d8fa013b299f0ba129d1e625bde2b
Renan Pereira <rpereira@glasslewis.com>
2024-07-02 00:07:46 +0100

    move to its own class the IDemo

---
+++ demo/DemoApp/IDemo.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoApp;
+
+public interface IDemo
+{
+    Task Run(CancellationToken cancellationToken = default);
+}
