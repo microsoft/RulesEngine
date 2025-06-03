71d59dce931d45eb9b9df97e35ac6721f0083352
Purunjay Bhal <purunjaybhal@microsoft.com>
2025-06-03 09:33:24 +0530

    [Bug Fix] Handling of Automatic Type Registration (#675)
    * adding support for automatic input-type registration
    
    * updating nuget version
    
    * updating code coverage

--- src/RulesEngine/CustomTypeProvider.cs
+++ src/RulesEngine/CustomTypeProvider.cs
@@ -3,7 +3,9 @@
 
 using RulesEngine.HelperFunctions;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Dynamic.Core;
 using System.Linq.Dynamic.Core.CustomTypeProviders;
 
@@ -11,16 +13,40 @@ namespace RulesEngine
 {
     public class CustomTypeProvider : DefaultDynamicLinqCustomTypeProvider
     {
-        private HashSet<Type> _types;
+        private readonly HashSet<Type> _types;
+
         public CustomTypeProvider(Type[] types) : base(ParsingConfig.Default)
         {
-            _types = new HashSet<Type>(types ?? new Type[] { });
+            _types = new HashSet<Type>(types ?? Array.Empty<Type>());
+
             _types.Add(typeof(ExpressionUtils));
+
+            _types.Add(typeof(Enumerable));
+
+            var queue = new Queue<Type>(_types);
+            while (queue.Count > 0)
+            {
+                var t = queue.Dequeue();
+
+                var baseType = t.BaseType;
+                if (baseType != null && _types.Add(baseType))
+                    queue.Enqueue(baseType);
+
+                foreach (var interfaceType in t.GetInterfaces())
+                {
+                    if (_types.Add(interfaceType))
+                        queue.Enqueue(interfaceType);
+                }
+            }
+
+            _types.Add(typeof(IEnumerable));
         }
 
         public override HashSet<Type> GetCustomTypes()
         {
-            return _types;
+            var all = new HashSet<Type>(base.GetCustomTypes());
+            all.UnionWith(_types);
+            return all;
         }
     }
 }
--- test/RulesEngine.UnitTest/CustomTypeProviderTests.cs
+++ test/RulesEngine.UnitTest/CustomTypeProviderTests.cs
@@ -3,7 +3,9 @@
 
 using Moq;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Xunit;
 
 namespace RulesEngine.UnitTest
@@ -12,33 +14,84 @@ namespace RulesEngine.UnitTest
     [ExcludeFromCodeCoverage]
     public class CustomTypeProviderTests : IDisposable
     {
-        private readonly MockRepository _mockRepository;
-        public CustomTypeProviderTests()
-        {
-            _mockRepository = new MockRepository(MockBehavior.Strict);
-        }
-
         public void Dispose()
         {
-            _mockRepository.VerifyAll();
         }
 
-        private CustomTypeProvider CreateProvider()
+        private CustomTypeProvider CreateProvider(params Type[] customTypes)
         {
-            return new CustomTypeProvider(null);
+            return new CustomTypeProvider(customTypes);
         }
 
         [Fact]
-        public void GetCustomTypes_StateUnderTest_ExpectedBehavior()
+        public void GetCustomTypes_DefaultProvider_IncludesEnumerableAndObject()
         {
-            // Arrange
-            var unitUnderTest = CreateProvider();
+            var provider = CreateProvider();
+            var allTypes = provider.GetCustomTypes();
+            Assert.NotEmpty(allTypes);
+            Assert.Contains(typeof(System.Linq.Enumerable), allTypes);
+            Assert.Contains(typeof(object), allTypes);
+        }
 
-            // Act
-            var result = unitUnderTest.GetCustomTypes();
+        [Fact]
+        public void GetCustomTypes_WithListOfGuid_ContainsIEnumerableOfGuid()
+        {
+            var initial = new[] { typeof(List<Guid>) };
+            var provider = CreateProvider(initial);
+            var allTypes = provider.GetCustomTypes();
+            Assert.Contains(typeof(IEnumerable<Guid>), allTypes);
+            Assert.Contains(typeof(List<Guid>), allTypes);
+            Assert.Contains(typeof(System.Linq.Enumerable), allTypes);
+            Assert.Contains(typeof(object), allTypes);
+        }
 
-            // Assert
-            Assert.NotEmpty(result);
+        [Fact]
+        public void GetCustomTypes_ListOfListString_ContainsIEnumerableOfListString()
+        {
+            var nestedListType = typeof(List<List<string>>);
+            var provider = CreateProvider(nestedListType);
+            var allTypes = provider.GetCustomTypes();
+            Assert.Contains(typeof(IEnumerable<List<string>>), allTypes);
+            Assert.Contains(nestedListType, allTypes);
+            Assert.Contains(typeof(System.Linq.Enumerable), allTypes);
+            Assert.Contains(typeof(object), allTypes);
+        }
+
+        [Fact]
+        public void GetCustomTypes_ArrayOfStringArrays_ContainsIEnumerableOfStringArray()
+        {
+            var arrayType = typeof(string[][]);
+            var provider = CreateProvider(arrayType);
+            var allTypes = provider.GetCustomTypes();
+            Assert.Contains(typeof(IEnumerable<string[]>), allTypes);
+            Assert.Contains(arrayType, allTypes);
+            Assert.Contains(typeof(System.Linq.Enumerable), allTypes);
+            Assert.Contains(typeof(object), allTypes);
+        }
+
+        [Fact]
+        public void GetCustomTypes_NullableIntArray_ContainsIEnumerableOfNullableInt()
+        {
+            var nullableInt = typeof(int?);
+            var arrayType = typeof(int?[]);
+            var provider = CreateProvider(arrayType);
+            var allTypes = provider.GetCustomTypes();
+            Assert.Contains(typeof(IEnumerable<int?>), allTypes);
+            Assert.Contains(arrayType, allTypes);
+            Assert.Contains(typeof(System.Linq.Enumerable), allTypes);
+            Assert.Contains(typeof(object), allTypes);
+        }
+
+        [Fact]
+        public void GetCustomTypes_MultipleTypes_NoDuplicates()
+        {
+            var repeatedType = typeof(List<string>);
+            var provider = CreateProvider(repeatedType, repeatedType);
+            var allTypes = provider.GetCustomTypes();
+            var matches = allTypes.Where(t => t == repeatedType).ToList();
+            Assert.Single(matches);
+            var interfaceMatches = allTypes.Where(t => t == typeof(IEnumerable<string>)).ToList();
+            Assert.Single(interfaceMatches);
         }
     }
 }
